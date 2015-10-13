b5d1c687f0f540e29af4b1d79dac6a67b074e2ab
asmita-sharma1625 <asmita_sharma@outlook.com>
2015-10-13 14:55:02 +0530

    adding demo logger to test rotator

---
+++ test/demo_logger.py
@@ -0,0 +1,20 @@
+from logger import Logger
+import time
+
+logger = Logger("demo_logger")
+
+def generate_logs():
+  i = 10000000
+  j = 2
+  while i > 0:
+    demo_log(j, j*2)
+    i = i - 1
+
+def demo_log(a, b):
+  logger.reportLatency("report latency", "demo-metric", demo_action, a, b)  
+
+def demo_action(a, b):
+  time.sleep(5)
+  return a / b
+  
+generate_logs()
