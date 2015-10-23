0116097f376287b34fa0ce394d45db520b74a9d1
Devender.Mishra <Devender.Mishra@ril.com>
2015-10-23 14:33:41 +0530

    Add default action to copy the files into the given directory

--- logger/consumer.py
+++ logger/consumer.py
@@ -3,13 +3,14 @@ import os
 import sqlite3
 import re
 import sys
+import shutil
 
 class Consumer:
 
   suffixpattern = "\d{4}-\d{2}-\d{2} [0-2][0-9]:[0-5][0-9]"
   fileextension = "metric.log"
 
-  class LastFileInfo:  
+  class LastFileInfo:
     infopath = "/tmp/lib/jiocloud-consumer/consumer.db"
 
     def open_fileinfos(self):
@@ -19,14 +20,14 @@ class Consumer:
           os.makedirs(os.path.dirname(Consumer.LastFileInfo.infopath))
         open(Consumer.LastFileInfo.infopath, "w").close()
         create_new = True
-  
+
       self.sqliteconnection = sqlite3.connect(Consumer.LastFileInfo.infopath)
       #Case where database is being created first time, table must be created.
       if create_new:
         c = self.sqliteconnection.cursor()
         c.execute("CREATE TABLE lastfiles (filename text primary key, lastfile text)")
         self.sqliteconnection.commit()
-  
+
     def close_fileinfos(self):
       self.sqliteconnection.close()
 
@@ -39,7 +40,7 @@ class Consumer:
       #print "Rows " + `rows`
       if rows:
         return rows[0]
-    
+
       cur.execute("INSERT INTO lastfiles (filename) VALUES('%s')"%(filename,))
       self.sqliteconnection.commit()
       return ""
@@ -56,9 +57,17 @@ class Consumer:
       return True
 
   class DefaultAction(ActionOnFile):
+    def __init__(self, target_path):
+      self.target_path = target_path
+
     def doTask(self, filename,logfilename):
-      myfile = open("/tmp/consumer.log","a")
-      myfile.write(filename+"\n")
+      dest_path = os.path.join(self.target_path, os.path.splitext(os.path.basename(logfilename))[0])
+      try:
+        if not os.path.isdir(dest_path):
+          os.makedirs(dest_path)
+        shutil.copy(filename, dest_path)
+      except:
+        return False
       return True
 
   class ObjectStorageAction(ActionOnFile):
@@ -66,15 +75,15 @@ class Consumer:
       self.username = "account_name:username"
       self.api_key = "API Public key."
       self.authurl = "https::/user.com/auth"
-      self.conn = cloudfiles.get_connection(username=self.username, api_key=self.api_key, authurl=self.authurl)
+      #self.conn = cloudfiles.get_connection(username=self.username, api_key=self.api_key, authurl=self.authurl)
 
     def doTask(self, filename, logfilename):
-      if not self.conn
-        return False
+      #if not self.conn
+      #  return False
       return True
 
   #def __init__(self, path, '''consumeinterval, ''' deleterotatedfiles=True, logpattern="", lastfileinfo=None):
-  def __init__(self, path, deleterotatedfiles=True, logpattern="", lastfileinfo=None, provider=None):
+  def __init__(self, path, deleterotatedfiles=True, logpattern="", provider = None, target_path=None, lastfileinfo=None):
     self.path = path
 #    self.consumerinterval = consumerinterval # Now it seems unneeded.
     self.deleterotatedfiles = deleterotatedfiles
@@ -88,10 +97,13 @@ class Consumer:
     else:
       self.logpattern = Consumer.suffixpattern
     self.regex = re.compile(self.logpattern)
+    dest_path = "/tmp/backups/"
+    if target_path:
+      dest_path = target_path
     if provider:
       self.provider = provider
     else:
-      self.provider = Consumer.DefaultAction()
+      self.provider = Consumer.DefaultAction(dest_path)
 
   def list_of_logs(self):
     return [os.path.join(dirpath, files) \
@@ -103,26 +115,20 @@ class Consumer:
       How to get list consumable files
       1. Estimate the last run time and picks file after that time.
       2. Get the last file which was processed. And filter out the files greater than that file. And update the file with increasing time.
-      Currently using method 2. Sqlite3 database is being used to 
+      Currently using method 2. Sqlite3 database is being used to
     '''
     fileprefix = os.path.basename(filename)
     lastfilename = self.lastfileinfo.get_last_file(filename)
     if lastfilename == "":
       lastfilename = fileprefix
 
-    #print "Last file "+lastfilename
-    ##print "File list "+`[os.path.join(dirpath, files) for (dirpath,_,filenames) in os.walk(os.path.dirname(filename)) \
-    #    for files in filenames if self.regex.search(files) and files > lastfilename]`
-    ##print "File list 2 "+`[os.path.join(dirpath, files) for (dirpath,_,filenames) in os.walk(os.path.dirname(filename)) \
-    #    for files in filenames]`
-    #filelists =  
     return sorted([os.path.join(dirpath, files) \
               for (dirpath, _, filenames) in os.walk(os.path.dirname(filename)) \
               for files in filenames if self.regex.search(files) and files.startswith(fileprefix) and files > lastfilename])
     #Update the data with the last file.
     #self.lastfileinfo.set_last_file(filename, filelists[-1])
     #return filelists
-    
+
 
   def do_action(self, last_value, filename):
     #This is just for test of now. Later, it will be upgraded for object storage.
@@ -147,11 +153,11 @@ class Consumer:
       self.lastfileinfo.set_last_file(filename, files[-1])
       if self.deleterotatedfiles:
         map(self.do_delete, files)
-  
+
   def consume(self):
     #Get list of file names
     file_names = self.list_of_logs()
-    
+
     #For each file name, get list of the files to consume
     self.lastfileinfo.open_fileinfos()
     map(self.consume_each_file, file_names)
--- test/test_consumer.py
+++ test/test_consumer.py
@@ -9,6 +9,7 @@ import tempfile
 import shutil
 import re
 import time
+import pdb
 
 class ConsumerTest(unittest.TestCase):
 
@@ -16,28 +17,33 @@ class ConsumerTest(unittest.TestCase):
     def doTask(self, filename, logfilename):
       myfile = open("/tmp/consumer.log","a")
       myfile.write(logfilename+': '+filename+"\n")
-      return True 
+      myfile.close()
+      return True
 
   def test_consumer(self):
     (tempath,filesize) = self.__create_setup()
     filecount = self.__validate_before(tempath)
-    os.unlink(consumer.Consumer.LastFileInfo.infopath)
+    if os.path.isfile(consumer.Consumer.LastFileInfo.infopath):
+      os.unlink(consumer.Consumer.LastFileInfo.infopath)
     logfiles = "/tmp/consumer.log"
-    os.unlink(logfiles)
+    if os.path.isfile(logfiles):
+      os.unlink(logfiles)
     self.assertTrue(filecount > 0)
 
     mrotator = rotator.Rotator(tempath)
     mrotator.rotate_log_file()
     time.sleep(2)
-    self.__create_setup_files(tempath) 
+    self.__create_setup_files(tempath)
     mrotator.rotate_log_file()
     time.sleep(1)
-    self.__create_setup_files(tempath) 
+    self.__create_setup_files(tempath)
     mrotator.rotate_log_file()
     time.sleep(1)
-    self.__create_setup_files(tempath) 
+    self.__create_setup_files(tempath)
     mrotator.rotate_log_file()
-    mconsumer = consumer.Consumer(tempath,True,"[0-9]+$", None, ConsumerTest.FileAction())
+    #pdb.set_trace()
+    mconsumer = consumer.Consumer(tempath,True,"[0-9]+$",ConsumerTest.FileAction())
+    #pudb.set_trace()
     mconsumer.consume()
     self.assertTrue(self.__wc_l(logfiles) == 20)
     self.__create_setup_files(tempath)
@@ -56,22 +62,86 @@ class ConsumerTest(unittest.TestCase):
     #self.assertTrue(self.__validate_after(tempath, filecount, filecount, filesize))
     self.__tear_setup(tempath)
 
+  def test_consumer2(self):
+    (tempath,filesize) = self.__create_setup_at("/tmp/tmph_FSBc/")
+    filecount = self.__validate_before(tempath)
+    if os.path.isfile(consumer.Consumer.LastFileInfo.infopath):
+      os.unlink(consumer.Consumer.LastFileInfo.infopath)
+    self.assertTrue(filecount > 0)
+
+    mrotator = rotator.Rotator(tempath)
+    #print "Initial Rotated file "+`self.__fc_l(tempath)`
+    mrotator.rotate_log_file()
+    #print "Initial Rotated file "+`self.__fc_l(tempath)`
+    time.sleep(2)
+    self.__create_setup_files(tempath)
+    mrotator.rotate_log_file()
+    #print "Initial Rotated file "+`self.__fc_l(tempath)`
+    time.sleep(1)
+    self.__create_setup_files(tempath)
+    mrotator.rotate_log_file()
+    #print "Initial Rotated file "+`self.__fc_l(tempath)`
+    time.sleep(1)
+    self.__create_setup_files(tempath)
+    mrotator.rotate_log_file()
+    #print "Initial Rotated file "+`self.__fc_l(tempath)`
+    #pdb.set_trace()
+    log_path = "/tmp/dev-test/"
+    #shutil.rmtree(log_path)
+    if not os.path.isdir(log_path):
+      os.makedirs(log_path)
+    mconsumer = consumer.Consumer(tempath,True,"[0-9]+$",None,log_path)
+    initial_files = self.__fc_l(log_path)
+    #print "Initial files "+`initial_files`
+    #pudb.set_trace()
+    mconsumer.consume()
+    #print "Number of files "+`self.__fc_l(log_path)`
+    self.assertTrue(self.__fc_l(log_path) == initial_files+20)
+    #initial_files += 20
+    self.__create_setup_files(tempath)
+    time.sleep(1)
+    mrotator.rotate_log_file()
+    #print "Initial Rotated file "+`self.__fc_l(tempath)`
+    mconsumer.consume()
+    #print "Number of files "+`self.__fc_l(log_path)`
+    self.assertTrue(self.__fc_l(log_path) == initial_files+25)
+    self.__create_setup_files(tempath)
+    time.sleep(1)
+    mconsumer.consume()
+    #print "Number of files "+`self.__fc_l(log_path)`
+    self.assertTrue(self.__fc_l(log_path) == initial_files+25)
+    time.sleep(1)
+    mrotator.rotate_log_file()
+    #print "Initial Rotated file "+`self.__fc_l(tempath)`
+    mconsumer.consume()
+    #print "Number of files "+`self.__fc_l(log_path)`
+    self.assertTrue(self.__fc_l(log_path) == initial_files+30)
+    #self.assertTrue(self.__validate_after(tempath, filecount, filecount, filesize))
+    self.__tear_setup(tempath)
+
+
   def __wc_l(self, filename):
     return open(filename, "r").read().count('\n')
-  
+
+  def __fc_l(self, folder):
+    return len([files for (dirp, dirname, filename) in os.walk(folder) for files in filename])
+
   def __create_setup(self):
     dirpath = tempfile.mkdtemp()
-    #print "Directory path " + dirpath
-    #Create a directory and two files inside it
-    os.makedirs(os.path.join(dirpath, "test_dir"))
-    filesize = self.__create_setup_files(dirpath)
-    return (dirpath, filesize)
+    return self.__create_setup_at(dirpath)
+
+  def __create_setup_at(self, path):
+    #/tmp/tmph_FSBc/
+    if not os.path.isdir(os.path.join(path, "test_dir")):
+      os.makedirs(os.path.join(path, "test_dir"))
+    filesize = self.__create_setup_files(path)
+    return (path, filesize)
 
   def __create_setup_files(self, dirpath):
     self.__write_contents(os.path.join(dirpath, "test_dir/mointor1_metric.log"))
     self.__write_contents(os.path.join(dirpath, "test_dir/service1_metric.log"))
     self.__write_contents(os.path.join(dirpath, "test_dir/service2_metric.log"))
-    
+
     #Create a file
     self.__write_contents(os.path.join(dirpath, "conductor_metric.log"))
     filesize = self.__write_contents(os.path.join(dirpath, "database_metric.log"))
