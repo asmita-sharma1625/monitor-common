d3765b4b25624ee381c19a50b432756a9d784ae9
asmita-sharma1625 <asmita_sharma@outlook.com>
2015-10-28 14:25:56 +0530

    adding test case for logFailure()

--- test/test_logger.py
+++ test/test_logger.py
@@ -12,14 +12,20 @@ class TestLogger(unittest.TestCase):
 
   def setUp(self):
     TestLogger.socket = TestLogger.socket + 1
-    configWriter.CreateConfigFile("config.cfg", "Constants", "Socket", "tcp://127.0.0.1:"+`TestLogger.socket`)
-    configWriter.CreateConfigFile("config.cfg", "Constants", "LogDir", ".")
-    configWriter.CreateConfigFile("config.cfg", "Constants", "Filename", "metric.log")
-    self.logger = logger.Logger(self.SERVICE, "config.cfg")
+    configWriter.CreateConfigFile("Config.cfg", "Constants", "Socket", "tcp://127.0.0.1:"+`TestLogger.socket`)
+    configWriter.CreateConfigFile("Config.cfg", "Constants", "LogDir", ".")
+    configWriter.CreateConfigFile("Config.cfg", "Constants", "Filename", "metric.log")
+    self.logger = logger.Logger(self.SERVICE, "Config.cfg")
 
   def dummy_func(self, a, b):
     return a / b  
 
+  def test_logFailure_fail(self):
+    self.assertEquals(self.logger.logFailure(self.NAME, self.MTYPE, 0), 0)
+
+  def test_logFailure_pass(self):
+    self.assertEquals(self.logger.logFailure(self.NAME, self.MTYPE, 1), 1)
+  
   def test_logIfFail_fail(self):
     self.assertEquals(self.logger.logIfFail(self.NAME, self.MTYPE, 2, 0, self.dummy_func, 4, 2), 0) 
 
