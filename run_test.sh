671180966995f222fb6ef2e087decd8b18c3ef88
asmita-sharma1625 <asmita_sharma@outlook.com>
2015-11-05 12:39:36 +0530

    dispalying test configs in run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -3,9 +3,15 @@ export PYTHONPATH="$CWD"
 echo $PYTHONPATH
 find |grep "pyc$"|xargs rm -f
  cd test
- rm *.pyc
+ rm *.pyc -f
+ rm *.cfg -f
  python -m unittest discover -s .
-
+ echo config.cfg
+ cat config.cfg
+ echo Config.cfg
+ cat Config.cfg
+ echo pconfig.cfg
+ cat pconfig.cfg  
 #cd $CWD
 #cd logger
 #python rotator.py /home/asmi/compute
