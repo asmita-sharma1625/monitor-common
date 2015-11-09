ff5383846a8535e0d3b0fa2ad9c395a2af6ddf81
asmita-sharma1625 <asmita_sharma@outlook.com>
2015-11-09 11:34:15 +0530

    port changed in test_logHandler

--- test/test_logHandler.py
+++ test/test_logHandler.py
@@ -10,7 +10,7 @@ from logger.common.configReader import ConfigReader
 class TestLogHandler(unittest.TestCase):
 
   SERVICE = "demo_service"
-  socket = 6765
+  socket = 5555
 
   def setUp(self):
     #pdb.set_trace()
